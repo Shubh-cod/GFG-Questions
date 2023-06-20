32e2da71715ed936ba583f7b8f0f310cce66f13c
Shubh-cod <82703280+Shubh-cod@users.noreply.github.com>
2023-06-20 19:45:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Valid Substring/valid-substring.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for c++
+class Solution {
+  public:
+    int findMaxLen(string s) {
+    stack<int>index;
+    stack<char>c;
+    index.push(-1);
+    int ansMax=0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i]=='(')
+        {
+            index.push(i);
+            c.push('(');
+        }
+        else
+        {
+            if (!c.empty() and c.top()=='(')
+            {
+                index.pop();
+                c.pop();
+                ansMax=max(ansMax,i-index.top());
+            }
+            else
+            {
+                index.push(i);
+            }
+        }
+    }
+    return ansMax;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S; 
+        Solution ob;
+        cout << ob.findMaxLen(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
